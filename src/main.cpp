068cd4fae01d60400115b469fff501e14385cc77
VarunVF <varunraviprojects@gmail.com>
2025-08-23 15:32:19 +0800

    basic opengl triangles rendering

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,95 @@
 #include "Window.hpp"
 
 
+const char* vertexShaderSource = 
+	"#version 330 core\n"
+	"layout (location = 0) in vec2 aPos;\n"
+	"\n"
+	"void main()\n"
+	"{\n"
+	"    gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);\n"
+	"}\n";
+
+const char* fragmentShaderSource =
+	"#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"\n"
+	"void main()\n"
+	"{\n"
+	"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\n";
+
+
 int main(void)
 {
 	Window window(640, 480, "title");
+
+	// Vertex positions in NDC range [-1.0, 1.0]
+	float vertices[] = {
+		-0.5f, -0.5f,
+		-0.5f,  0.5f,
+		 0.5f, 0.5f,
+		 0.5f, -0.5f,
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		0, 2, 3,
+	};
+
+	const int indicesCount = sizeof(indices) / sizeof(indices[0]);
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int IBO;
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glUseProgram(shaderProgram);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Unbind our settings
+	glBindVertexArray(0);
+
+	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	while (!window.shouldClose())
 	{
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, indicesCount, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
 		window.swapBuffers();
 		window.pollEvents();
 	}
